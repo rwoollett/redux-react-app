7ccf562e7a858f3aec073425d104dc18ce693dad
Rodney Woollett <rodney.woollett@gmail.com>
2025-07-20 19:17:34 +1200

    searchImage

--- src/utility/searchImage.ts
+++ src/utility/searchImage.ts
@@ -3,7 +3,7 @@ import { ImageData } from "../types/imageData";
 const searchImages = async (term: string): Promise<ImageData[]> => {
   const response = await fetch(`https://api.unsplash.com/search/photos?query=${term}`, {
     headers: {
-      Authorization: 'Client-ID WczPny0E8s0Uao8iOtnxW8i1lEH4VkX1bUqRAdjNPpo'
+      Authorization: 'Client-ID '
     },
   });
   const data = await response.json();
