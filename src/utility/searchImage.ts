6a8cbcc1a7a08802120e8567ebf8c9c79917265f
Rodney Woollett <rodney.woollett@gmail.com>
2025-07-20 19:31:31 +1200

    searchImage

--- src/utility/searchImage.ts
+++ src/utility/searchImage.ts
@@ -3,7 +3,7 @@ import { ImageData } from "../types/imageData";
 const searchImages = async (term: string): Promise<ImageData[]> => {
   const response = await fetch(`https://api.unsplash.com/search/photos?query=${term}`, {
     headers: {
-      Authorization: 'Client-ID '
+      Authorization: `Client-ID ${process.env.REACT_APP_SPLASH_KEY}`
     },
   });
   const data = await response.json();
